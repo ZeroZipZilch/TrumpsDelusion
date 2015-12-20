cb8d071bf9f951209251cb5af619950193204d1f
Nemo <nemoxias@nemoxias.fairyland>
2015-12-21 00:16:00 +0100

    Added is canvas supported method

--- scripts/Game.js
+++ scripts/Game.js
@@ -127,7 +127,7 @@ Game.prototype.preLoadImages = function()
     isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
     isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
 
-	if(isChrome)
+	if(isChrome && !isSafari)
 	{
 		context.fillStyle = "#fafafa";
 		context.fillRect(0,0,canvas.width,canvas.height);
